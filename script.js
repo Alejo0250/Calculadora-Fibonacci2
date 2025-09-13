3a7de478bc372ebf528d6f2057785f4d08152bf2
alejo cangrejo <alejandritodlcruz@gmail.com>
2025-09-13 08:54:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-    function gen() {
+// Selecciona el elemento con el id "menu-icon" y agrega un evento de clic
+document.getElementById("menu-icon").addEventListener("click", function () {
+  // Dentro del evento de clic, selecciona el elemento con el id "menu"
+  const menu = document.getElementById("menu");
+
+  // Alterna la clase "hidden" en el elemento "menu"
+  // Si la clase "hidden" está presente, la elimina; si no está, la agrega
+  menu.classList.toggle("hidden");
+});
+
+function gen() {
       const size = parseInt(document.getElementById("size").value);
       const fib = [0n, 1n];
       let res = "";
